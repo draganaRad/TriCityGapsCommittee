d0817a81156cc0d1837aa6b303ad958952d05f76
Dragana Radulovic <dradulovic@Draganas-MBP.vc.shawcable.net>
2023-08-22 21:11:39 -0700

    added data to HUBemail layer

--- data/HUBemail.js
+++ data/HUBemail.js
@@ -3,7 +3,7 @@ var HUBemail = {
   "features": [
     {
       "type": "Feature",
-      "id": "C_5",
+      "id": "C_1",
       "properties": {
         "email":"true",
         "key": "C",
@@ -11,10 +11,10 @@ var HUBemail = {
         "type": "Problem",
         "location": "Coquitlam, Glen Dr. and Pipeline Rd. near Glen Elementary",
         "description": "There are raised bollards at the corner of Glen Dr. and Pipeline Rd. near Glen Elementary. Can 1 or 2 of the bollards nearest the sidewalk be removed to allow cyclists to bike on the right of the road and access the crosswalk activation sensor? I’ve attached a photo of the area with circles around the areas that need to be addressed.<br>It is currently impossible for cyclists to turn left safely, or indeed legally, from Glen Dr (a city designated bike path) onto Pipeline Rd. as cyclists cannot trigger a green light. The crosswalk sensor is unreachable from the road. Cyclists cannot enter the bollard area to reach the lighting pole where the sensor is mounted. Moreover, the road does not have a bike sensor to generate a green light.<br>If the highlighted bollards are removed, cyclists can continue on the right side of the Glen, cross Pipeline alongside the crosswalk, and turn left once they’ve reached the eastern side of Pipeline.<br>Can this change be prioritized? Glen Dr. is a designated bike lane by the city. In addition, the road also serves the elementary school. I think that this minor change would significantly improve the safety of the cycling path so that it is accessible by all ages, and promote more active transportation to the school.",
-        "photo": "2.jpg",
+        "photo": "1.jpg",
         "date": "2022-11-03",
         "descriptionUpdate": "(Feb 2023) Bollards nearest the sidewalk removed.",
-        "photoUpdate": "2a.png"
+        "photoUpdate": "1a.png"
       },
       "geometry": {
         "type": "Point",
@@ -23,6 +23,175 @@ var HUBemail = {
           49.282643
         ]
       }
-    }
+    },
+    {
+      "type": "Feature",
+      "id": "C_2",
+      "properties": {
+        "email":"true",
+        "key": "C",
+        "city": "Coquitlam",
+        "type": "Problem",
+        "location": "Coquitlam, Myrna St & Lougheed Highway",
+        "description": "Believe it or not I cycle from Vancouver to my office in Langley Routinely. I have seen many cars drive straight through this bike only lane in my time cycling, but always either in front of me, or slowed down and came in behind me.<br>Last night as I was in a bright yellow reflective Jacket, Orange Helmet, Flashing red rear light, Headlamp, and Front flashing light a vehicle came from behind through there at over 60kmh while I was in between the pylons and the island.<br>Luckily a swerved into the island and was only slightly brushed by the car and my bike was not damaged. The person stopped and was very apologetic and had no idea that they were not allowed to drive through there, and also claims they did not see me.<br>Can you please do something to block this so cars cannot go through, or remove the pylons or anything before someone is killed.",
+        "photo": "2.jpeg",
+        "date": "2022-11-29"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.849224,
+          49.232046
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "C_3",
+      "properties": {
+        "email":"true",
+        "key": "C",
+        "city": "Coquitlam, 613 Nicola Ave",
+        "type": "Problem",
+        "location": "Coquitlam, 613 Nicola Ave",
+        "description": "Removing maze gates (aka baffles) between Nicola and Chapman. Is there a timeline for removing these?",
+        "photo": "3.png",
+        "date": "2022-02-11"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.887019,
+          49.269099
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "C_4",
+      "properties": {
+        "email":"true",
+        "key": "C",
+        "city": "Coquitlam",
+        "type": "Problem",
+        "location": "Coquitlam, Nicola Ave 500 to 600 block",
+        "description": "The city-owned 3-metre-wide accessway between the 500 block and 600 block of Nicola Ave is overgrown and needs to be cleared for pedestrian, stroller and cycling access.",
+        "photo": "4.png",
+        "date": "2022-11-03"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.888881,
+          49.268943
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "PM_1",
+      "properties": {
+        "email":"true",
+        "key": "PM",
+        "city": "Port Moody",
+        "type": "Problem",
+        "location": "Port Moody, Clark Street",
+        "description": "I\'m writing about the possibility of protected bike lanes on Clarke Street in Port Moody. This would be an amazing improvement to the existing infrastructure in that area. Currently the only designated bike route connecting the Murray bike lane to the Barnet bike lane is on Spring Street which is awful for riding a bicycle, including poor surfacing, stop signs at every block, bad grade breaks at each intersection, and poor lighting.<br>Many months ago I stopped riding on Spring and now only ride on Clarke, and many others do as well because Clarke is much smoother and much faster than Spring. Having protected infrastructure on Clarke would make riding a bicycle safer and entice more people to ride bicycles instead of driving.<br>The motor vehicle traffic jams that occur regularly along St Johns and Clarke are already awful, so alternate modes of transportation are important as the population of Port Moody and all of the Tri-Cities increases.",
+        "photo": "",
+        "date": "2023-02-15"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.850234,
+          49.277773
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "PM_2",
+      "properties": {
+        "email":"true",
+        "key": "PM",
+        "city": "Port Moody",
+        "type": "Problem",
+        "location": "Port Moody, Barnet Highway",
+        "description": "I pass through Port Moody daily on my commute from Port Coquitlam to Burnaby and back on my bicycle. It is always a relief to get to Burnaby or Coquitlam (depending on direction). The Port Moody section of Barnet highway is consistently dangerous. There is organic debris and damaged automobile components along this stretch in both directions. There is poor lighting.<br>Once, on a dark and stormy morning, I crashed into a fallen tree, which I reported and was never address by city crews. Myself, and presumably others that use the stretch did the best we could with our resources to reduce the risk. That prompted other risks and motorists often speed, use the HOV as a passing lane, and some just genuinely lack empathy for other road users.<br>I would be happy if the processes for checking and maintaining this infrastructure could be reviewed and improved.",
+        "photo": "",
+        "date": "2023-02-01"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.872523,
+          49.290330
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "C_5",
+      "properties": {
+        "email":"true",
+        "key": "C",
+        "city": "Coquitlam",
+        "type": "Problem",
+        "location": "Coquitlam, 1961-1939 Lougheed Hwy",
+        "description": "This is a stretch of Lougheed Hwy westbound that I ride regularly on my commute home and over the summer, debris has been laid across this bit of road at least 6 times. I have attached the location on a map screenshot.<br>The first time I saw it, I cleared it out of the way and then the next time I rode by it was back. I clear it (I/’ve even thrown it into the ditch) and then it keeps reappearing. The reason I think it is intentional is because the rocks and wood are laid evenly across the path.",
+        "photo": "5.jpg",
+        "date": "2022-10-08"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.842256,
+          49.230592
+        ]
+      }
+     }
+    //,
+    // {
+    //   "type": "Feature",
+    //   "id": "C_3",
+    //   "properties": {
+    //     "email":"true",
+    //     "key": "C",
+    //     "city": "Coquitlam",
+    //     "type": "Problem",
+    //     "location": "",
+    //     "description": "",
+    //     "photo": "3.jpeg",
+    //     "date": "2022-11-03"
+    //   },
+    //   "geometry": {
+    //     "type": "Point",
+    //     "coordinates": [
+    //       -122.849224,
+    //       49.232046
+    //     ]
+    //   }
+    // },
+    // {
+    //   "type": "Feature",
+    //   "id": "C_3",
+    //   "properties": {
+    //     "email":"true",
+    //     "key": "C",
+    //     "city": "Coquitlam",
+    //     "type": "Problem",
+    //     "location": "",
+    //     "description": "",
+    //     "photo": "3.jpeg",
+    //     "date": "2022-11-03"
+    //   },
+    //   "geometry": {
+    //     "type": "Point",
+    //     "coordinates": [
+    //       -122.849224,
+    //       49.232046
+    //     ]
+    //   }
+    // }
   ]
 }
